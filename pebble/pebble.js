18e05c5a6de1506f220cabdab8a2c683bcc0e9d5
YojouhanSeikatsu <rikusaito64@gmail.com>
2026-01-26 19:17:04 -0500

    updated keptkeys

--- pebble/pebble.js
+++ pebble/pebble.js
@@ -1545,7 +1545,7 @@ function sendMessage() {
             } else if (message == "!cleardonations") {
                 if (obj.admin > 9000 || Object.hasOwn(otherObject.val().admin_list, user_object.val().id)) {
                     db.ref(`users/`).once("value", function(data_clear) {
-                        const keptKeys = ["active", "admin", "muted", "name", "password", "sleep", "username", "xss", "trapped", "profilesleep", "active_effect", "effects", "display_name", "donationsban", "activeoption", "shadowban", "forceverify"];
+                        const keptKeys = ["active", "admin", "muted", "name", "password", "sleep", "username", "xss", "trapped", "profilesleep", "active_effect", "effects", "display_name", "donationsban", "activeoption", "shadowban", "forceverify", "fingerprint"];
                         var updates = {};
 
                         data_clear.forEach(child => {

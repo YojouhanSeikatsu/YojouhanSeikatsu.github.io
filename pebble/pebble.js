bee0de69eb1c616c9748c868aae6d6bc8250b641
YojouhanSeikatsu <rikusaito64@gmail.com>
2026-01-30 22:09:47 -0500

    got rid of loading chat using button

--- pebble/pebble.js
+++ pebble/pebble.js
@@ -16,8 +16,6 @@ var globalActive;
 let db, auth, storage, requestId;
 
 function getChats() {
-    document.getElementById("getChatsButton").remove();
-    
     db.ref(`users/${getUsername()}`).once("value", function(user_object) {
         db.ref('chats/').on('child_added', function(message_object) {
             globalMessages.push(message_object);
@@ -2082,6 +2080,7 @@ function setup() {
             checkDeletion();
             checkEdit();
             checkMute();
+            getChats();
 
             db.ref("other/medianAdmin").on('value', (obj) => {
                 obj = obj.val();
